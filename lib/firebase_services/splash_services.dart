52971027c84fa8f528eeed53c9ab98bde79bc4ca
JayaramDhungana <[jayaramdhungana104@gmail.com]git config --global user.email [jayaramdhungana104@gmail.com]>
2025-07-10 09:16:54 +0545

    check if user is already logged in

--- lib/firebase_services/splash_services.dart
+++ lib/firebase_services/splash_services.dart
@@ -1,17 +1,33 @@
 import 'dart:async';
 
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_practice/UI/login_screen.dart';
+import 'package:firebase_practice/UI/posts/post_screen.dart';
 import 'package:firebase_practice/UI/splash_screen.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 
 class SplashServices {
+  final auth = FirebaseAuth.instance;
+
   isLogin(BuildContext context) {
-    Timer(Duration(seconds: 5), () {
-      Navigator.push(
-        context,
-        MaterialPageRoute(builder: (context) => LoginScreen()),
-      );
-    });
+    final user = auth.currentUser;
+
+    if (user != null) {
+      Timer(Duration(seconds: 5), () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => PostScreen()),
+        );
+      });
+    } else {
+      Timer(Duration(seconds: 5), () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => LoginScreen()),
+        );
+      });
+    }
   }
 }
